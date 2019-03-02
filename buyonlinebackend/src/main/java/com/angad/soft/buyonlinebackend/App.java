18643b8a92ee631c445613fb71a36a486d53e729
angad jha <angadjava@gmail.com>
2019-03-02 17:20:29 +0530

    added class

---
+++ buyonlinebackend/src/main/java/com/angad/soft/buyonlinebackend/App.java
@@ -0,0 +1,13 @@
+package com.angad.soft.buyonlinebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
